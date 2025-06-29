4fa4aac41687f25f892d5d1a02f59d09fe74684a
rajisafiu <rajikolawole677@gmail.com>
2025-06-29 17:54:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+    // function withdrawFunds() {
+    //   alert("How much are you withdrawing?");
+    // }
+
+    // function addTransaction() {
+    //   alert("how much are you depositing?");
+    // }
+
+    // function addAirtimedata() {
+    //   alert("Are you buying airtime or Data?");
+    // }
+
+
+
+
