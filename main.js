b424aa0ac00b80cbfdfa93437594cf387f45be47
rajisafiu <rajikolawole677@gmail.com>
2025-07-02 16:27:41 +0100

    upadte on transaction history,dark mode & light mode and also do some litte work on java script

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-    // function withdrawFunds() {
-    //   alert("How much are you withdrawing?");
-    // }
+   
+//    main dashboard below
 
-    // function addTransaction() {
-    //   alert("how much are you depositing?");
-    // }
+   function withdrawFunds() {
+      alert("How much are you withdrawing?");
+    }
 
-    // function addAirtimedata() {
-    //   alert("Are you buying airtime or Data?");
-    // }
+    function addTransaction() {
+      alert("how much are you depositing?");
+    }
+
+    function addAirtimedata() {
+      alert("Are you buying airtime or Data?");
+    }
+
+    // transaction history below
+
+    document.getElementById("typeFilter").addEventListener("change", function() {
+      alert("Filter type: " + this.value);
+    });
+
+    document.getElementById("dateFilter").addEventListener("change", function() {
+      alert("Filter date: " + this.value);
+    });
+
+    // dark mode and light mode below
+
+        const toggleBtn = document.getElementById("toggleMode");
+    const body = document.body;
+
+    toggleBtn.addEventListener("click", () => {
+      body.classList.toggle("dark-mode");
+
+      const isDark = body.classList.contains("dark-mode");
+      toggleBtn.textContent = isDark ? "🌞" : "🌙";
+    }); 
 
 
 
